f6454c6022443169aa175e7c21a915a47980c7ee
Garda6 <info@gardainformatica.it>
2024-11-12 15:43:42 +0100

    creato buildDataset.py

--- codes/buildDataset.py
+++ codes/buildDataset.py
@@ -1,2 +1,22 @@
 import os 
-def build(promptDir):
+
+#baseString = '{"messages": [{"role": "system", "content": ""}, {"role": "user", "content": ""}, {"role": "assistant", "content": ""}]}'
+firstPiece='{"messages": [{"role": "system", "content": "'
+secondPiece='}, {"role": "user", "content": "'
+thirdPiece='"}, {"role": "assistant", "content": "'
+fourthPiece='"}]}'
+jsonForm='{"Comune":null, "Indirizzo":null, "Vani": null, "Locali": null,  "Mq": null, "Bagni": null, "Piano": null, "Posti auto": null, "N° Procedura": null, "Lotto":null}'
+def build(system, promptDir, assistantDir):
+    result=open('result.txt', 'w', encoding='UTF-8')
+    for subdir, dirs, files in os.walk(promptDir):
+        for file in files:
+            print(file)
+            userMsg = open(subdir+'/'+file, 'r', encoding='UTF-8')
+            result.write(firstPiece+system+secondPiece+userMsg.read()+thirdPiece+jsonForm+fourthPiece)
+    result.close()
+
+
+system=input('inserire file system: ')
+promptDir=input('inserire promptDir: ')
+build(system, promptDir, jsonForm)
+print('end')
