2065867b6ec091cdcfaa2b03fcf296541835c5f0
Garda6 <info@gardainformatica.it>
2024-11-27 10:36:22 +0100

    corretti bug checkXML.py. Aggiunti test chatgpt-4o-mini

--- codes/checkXML.py
+++ codes/checkXML.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from xml.parsers.expat import ExpatError
 import xmltodict
 
 def countPoints(an, rAn):
@@ -9,8 +10,10 @@ def countPoints(an, rAn):
             if isinstance(v1, dict) and isinstance(v2, dict):
                 points = points + countPoints(v1, v2)
             elif isinstance(v1, list) and isinstance(v2, list):
-                for i in range(0, len(v1)):
-                    points = points + countPoints(v1[i], v2[i])
+                if len(v1)==len(v2):
+                    for i in range(0, len(v1)):
+                        points = points + countPoints(v1[i], v2[i])
+                else: return 0
             else:
                 if k1==k2:
                     if v1==v2: points += 1
@@ -31,14 +34,17 @@ def maxPoints(rAn):
 
 def check(answer, rightAnswer):
     if os.path.splitext(answer)[1] == '.xml' and os.path.splitext(rightAnswer)[1] == '.xml':
-        with open(answer, 'r') as answer:
-            answer = xmltodict.parse(answer.read())
-        with open(rightAnswer, 'r') as rightAnswer:
-            rightAnswer = xmltodict.parse(rightAnswer.read())
+        try: 
+            with open(answer, 'r') as answer:
+                answer = xmltodict.parse(answer.read())
+            with open(rightAnswer, 'r') as rightAnswer:
+                rightAnswer = xmltodict.parse(rightAnswer.read())
+        except ExpatError:
+            return 'bad-formatted xml file'
     else: return 'wrong type of file'
     if answer.keys() == rightAnswer.keys():
-        print(countPoints(answer, rightAnswer))
-        print(maxPoints(rightAnswer))
+        #print(countPoints(answer, rightAnswer))
+        #print(maxPoints(rightAnswer))
         pt=(countPoints(answer, rightAnswer)/maxPoints(rightAnswer))*100
         if pt < 0: pt = 0
         return '{:.2f}%'.format(pt)
@@ -52,6 +58,7 @@ for subdir1, dirs1, files1 in os.walk(answersDir):
     for subdir2, dirs2, files2 in os.walk(rightDir):
         for ans in files1:
             for rightAns in files2:
-                if ans==rightAns: print(check(subdir1+'/'+ans, subdir2+'/'+rightAns))
+                if ans==rightAns: 
+                    print(ans+': '+check(subdir1+'/'+ans, subdir2+'/'+rightAns))
     
 	
