af484aa7d222b24fba0d98adba53a271f27a0d23
Garda6 <info@gardainformatica.it>
2024-12-09 10:49:51 +0100

    per sicurezza

--- codes/bash-scripts/install.sh
+++ codes/bash-scripts/install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-appList="pip p7zip-full zip unzip huggingface_hub[cli]"
-pipList="langchain langchain-ollama langchain-experimental"
+appList="pip p7zip-full zip unzip"
+pipList="langchain langchain-ollama langchain-experimental transformers datasets evaluate accelerate torch huggingface_hub[cli]"
 sudo apt update
 sudo apt upgrade
 sudo apt install linux-headers-amd64
 sudo apt install nvidia-driver firmware-misc-nonfree
 sudo apt-get -y install $appList
-pip install $pipList
+pip install -U  $pipList
 
 curl -fsSL https://ollama.com/install.sh | sh
 
-touch ~/.bash_aliases
-echo "alias python=python3" >> ~/.bash_aliases
-#echo PATH=$PATH:/my/dir >> ~/.bashrc
-source ~/.bash_aliases
-source ~/.bashrc
+FILE=~/.bash_aliases
+if [ ! -f "$FILE" ]; then
+	touch $FILE
+	echo "alias python=python3" >> $FILE
+	#echo PATH=$PATH:/my/dir >> ~/.bashrc
+	source $FILE
+	source ~/.bashrc
+fi
 
