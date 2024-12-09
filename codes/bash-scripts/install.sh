9829cfc52f8f499c77328cc9ad39bd44c0cff3da
Garda6 <info@gardainformatica.it>
2024-12-09 11:07:04 +0100

    per sicurezza

--- codes/bash-scripts/install.sh
+++ codes/bash-scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+usr=$USER
 appList="pip p7zip-full zip unzip"
-pipList="langchain langchain-ollama langchain-experimental transformers datasets evaluate accelerate torch huggingface_hub[cli]"
+pipList="langchain langchain-ollama langchain-experimental transformers datasets evaluate accelerate torch torchvision torchao torchtune  huggingface_hub[cli]"
 sudo apt update
 sudo apt upgrade
 sudo apt install linux-headers-amd64
@@ -15,7 +15,7 @@ FILE=~/.bash_aliases
 if [ ! -f "$FILE" ]; then
 	touch $FILE
 	echo "alias python=python3" >> $FILE
-	#echo PATH=$PATH:/my/dir >> ~/.bashrc
+	echo PATH=$PATH:/home/$usr/.local/bin >> ~/.bashrc
 	source $FILE
 	source ~/.bashrc
 fi
