90b337d54e90fe1ae512c9049b51894d23d3020c
Garda6 <info@gardainformatica.it>
2024-12-18 12:04:44 +0100

    aggiunto ollama-request.sh

---
+++ bash-scripts/request.sh
@@ -0,0 +1,10 @@
+MODEL_DIR=$1
+FILE=$2
+OUTPUT_FILENAME=$3
+TMP=tmp.json
+FORMAT='{ "type": "object", "properties": { "Immobili": { "type": "object", "properties": { "Comune": { "type": "string" }, "Indirizzo": { "type": "string" }, "Vani": { "type": "number" }, "Locali": { "type": "integer" }, "Mq": { "type": "number" }, "Bagni": { "type": "integer" }, "Piano": { "type": "string" }, "Posti auto": { "type": "string" }, "N° Procedura": { "type": "string" }, "Lotto": { "type": "string" } }, "required": [ "Comune", "Indirizzo", "Vani", "Locali", "Mq", "Bagni", "Piano", "Posti auto", "N° Procedura", "Lotto" ] }} }'
+FILE=$( cat $FILE | jq -Rsa . )
+DATA='{ "model": "test", "modelfile": "'$MODEL_DIR'", "prompt": '$FILE', "stream":false, "format": '$FORMAT' }'
+echo $DATA > $TMP
+curl http://localhost:20000/api/generate -d @tmp.json > $OUTPUT_FILENAME
+rm $TMP
