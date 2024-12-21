5ff7fa75f923e12dfb7a4f3bd674ab0452c2ccdc
Daniel Acampora <dani00aca@gmail.com>
2024-12-21 16:57:33 +0100

    risolto problema output strutturato

--- bash-scripts/request.sh
+++ bash-scripts/request.sh
@@ -2,9 +2,9 @@ MODEL_DIR=$1
 FILE=$2
 OUTPUT_FILENAME=$3
 TMP=tmp.json
-FORMAT='{ "type": "object", "properties": { "Immobili": { "type": "object", "properties": { "Comune": { "type": "string" }, "Indirizzo": { "type": "string" }, "Vani": { "type": "number" }, "Locali": { "type": "integer" }, "Mq": { "type": "number" }, "Bagni": { "type": "integer" }, "Piano": { "type": "string" }, "Posti auto": { "type": "string" }, "N° Procedura": { "type": "string" }, "Lotto": { "type": "string" } }, "required": [ "Comune", "Indirizzo", "Vani", "Locali", "Mq", "Bagni", "Piano", "Posti auto", "N° Procedura", "Lotto" ] }} }'
+FORMAT='{"type":"array","items":{"type":"object","properties":{"Comune":{"type":"string"},"Indirizzo":{"type":"string"},"Vani":{"type":"number"},"Locali":{"type":"integer"},"Mq":{"type":"number"},"Bagni":{"type":"integer"},"Piano":{"type":"string"},"Posti auto":{"type":"string"},"N° Procedura":{"type":"string"},"Lotto":{"type":"string"}},"required":["Comune","Indirizzo","Vani","Locali","Mq","Bagni","Piano","Posti auto","N° Procedura","Lotto"]}}'
 FILE=$( cat $FILE | jq -Rsa . )
 DATA='{ "model": "test", "modelfile": "'$MODEL_DIR'", "prompt": '$FILE', "stream":false, "format": '$FORMAT' }'
 echo $DATA > $TMP
-curl http://localhost:20000/api/generate -d @tmp.json > $OUTPUT_FILENAME
+curl http://localhost:11434/api/generate -d @tmp.json > $OUTPUT_FILENAME
 rm $TMP
